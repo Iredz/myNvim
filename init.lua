0b1ee2a4aae165cf32d64253cade7f57e1a3d501
Oscar I. Rojas <oscarrojas991@protonmail.com>
2026-04-21 15:52:49 -0500

    Tweak: Added better performance config for gruvbox-material

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ vim.pack.add({
 vim.g.gruvbox_material_enable_italic = true
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_better_performance = 1
 
 vim.cmd.colorscheme("gruvbox-material")
 vim.g.live_server = {
