ae0ea2adc9cbd68bb012290ee9737634a55c48c2
Oscar I. Rojas <oscarrojas991@protonmail.com>
2026-05-19 12:02:02 -0500

    Feat: Init.lua cleanup

--- init.lua
+++ init.lua
@@ -1,140 +1,138 @@
 require("configs.options")
 vim.pack.add({
-    --  COLORSCHEMES --
-    { src = "https://github.com/catppuccin/nvim" },
-    { src = "https://github.com/gbprod/nord.nvim" },
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/folke/tokyonight.nvim" },
-    { src = "https://github.com/oxfist/night-owl.nvim" },
-    { src = "https://github.com/craftzdog/solarized-osaka.nvim" },
-    { src = "https://github.com/alexvzyl/nordic.nvim" },
-    { src = "https://github.com/sainnhe/gruvbox-material" },
-    { src = "https://github.com/olimorris/onedarkpro.nvim" },
-    { src = "https://github.com/wurli/cobalt.nvim" },
-    -- LSP --
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = 'https://github.com/williamboman/mason-lspconfig.nvim' },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    {
-        src = "https://github.com/saghen/blink.cmp",
-        version = vim.version.range("1.*")
-    },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
-    {
-        src = "https://github.com/nvim-treesitter/nvim-treesitter",
-        hooks = { post_install = 'TSUpdate' }
-    },
-    -- EDITOR --
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/saghen/blink.indent" },
-    { src = "https://github.com/j-hui/fidget.nvim" },
-    { src = "https://github.com/folke/lazydev.nvim" },
-    { src = "https://github.com/nvim-mini/mini.nvim" },
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },
-    { src = "https://github.com/folke/trouble.nvim" },
-    { src = "https://github.com/akinsho/toggleterm.nvim" },
-    { src = "https://github.com/Bekaboo/dropbar.nvim" },
-    { src = "https://github.com/folke/snacks.nvim" },
-    { src = "https://github.com/folke/todo-comments.nvim" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://git.barrettruth.com/barrettruth/live-server.nvim" },
-    { src = "https://github.com/aserowy/tmux.nvim" },
+	--  COLORSCHEMES --
+	{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/gbprod/nord.nvim" },
+	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/oxfist/night-owl.nvim" },
+	{ src = "https://github.com/craftzdog/solarized-osaka.nvim" },
+	{ src = "https://github.com/alexvzyl/nordic.nvim" },
+	{ src = "https://github.com/sainnhe/gruvbox-material" },
+	{ src = "https://github.com/olimorris/onedarkpro.nvim" },
+	{ src = "https://github.com/wurli/cobalt.nvim" },
+	-- LSP --
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/williamboman/mason-lspconfig.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{
+		src = "https://github.com/saghen/blink.cmp",
+		version = vim.version.range("1.*"),
+	},
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		hooks = { post_install = "TSUpdate" },
+	},
+	-- EDITOR --
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/saghen/blink.indent" },
+	{ src = "https://github.com/j-hui/fidget.nvim" },
+	{ src = "https://github.com/folke/lazydev.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/folke/trouble.nvim" },
+	{ src = "https://github.com/akinsho/toggleterm.nvim" },
+	{ src = "https://github.com/Bekaboo/dropbar.nvim" },
+	{ src = "https://github.com/folke/snacks.nvim" },
+	{ src = "https://github.com/folke/todo-comments.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://git.barrettruth.com/barrettruth/live-server.nvim" },
+	{ src = "https://github.com/aserowy/tmux.nvim" },
 })
 
-vim.cmd.colorscheme("cobalt")
+vim.cmd.colorscheme("nordic")
 
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_foreground = "material"
-vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 vim.g.gruvbox_material_better_performance = 1
 
 require("onedarkpro").setup({
-    options = {
-        transparency = false
-    },
-    highlights = {
-        Comment = { italic = false, bold = false },
-        Directory = { bold = true },
-        ErrorMsg = { italic = true, bold = true }
-    },
-    styles = {
-        types = "bold",
-        methods = "bold",
-        numbers = "NONE",
-        strings = "NONE",
-        comments = "italic",
-        keywords = "bold,italic",
-        constants = "italic",
-        functions = "bold,italic",
-        operators = "NONE",
-        variables = "NONE",
-        parameters = "NONE",
-        conditionals = "italic",
-        virtual_text = "NONE",
-    }
+	options = {
+		transparency = false,
+	},
+	highlights = {
+		Comment = { italic = false, bold = false },
+		Directory = { bold = true },
+		ErrorMsg = { italic = true, bold = true },
+	},
+	styles = {
+		types = "bold",
+		methods = "bold",
+		numbers = "NONE",
+		strings = "NONE",
+		comments = "italic",
+		keywords = "bold,italic",
+		constants = "italic",
+		functions = "bold,italic",
+		operators = "NONE",
+		variables = "NONE",
+		parameters = "NONE",
+		conditionals = "italic",
+		virtual_text = "NONE",
+	},
 })
 
 vim.g.live_server = {
-    port = 8080,
-    browser = true
+	port = 8080,
+	browser = true,
 }
 
 require("todo-comments").setup()
 require("catppuccin").setup({
-    transparent_background = false,
+	transparent_background = false,
 })
 require("tokyonight").setup()
 
 require("snacks").setup({
-    lazygit = {
-        configure = true,
-    },
-    picker = {
-        enabled = true,
-        ui_select = true,
-        previewers = {
-            diff = {
-                style = "fancy"
-            }
-        },
-        preset = "ivy",
-    }
+	lazygit = {
+		configure = true,
+	},
+	picker = {
+		enabled = true,
+		ui_select = true,
+		previewers = {
+			diff = {
+				style = "fancy",
+			},
+		},
+		preset = "ivy",
+	},
 })
 
 require("dropbar").setup()
 require("gitsigns").setup({
-    signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "-" },
-        changedelete = { text = "+" },
-    }
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "-" },
+		changedelete = { text = "+" },
+	},
 })
 
-require("trouble").setup({
-
-})
+require("trouble").setup({})
 require("toggleterm").setup({
-    open_mapping = [[<c-\>]],
-    direction = "float"
+	open_mapping = [[<c-\>]],
+	direction = "float",
 })
 
 require("fidget").setup({
-    notification = {
-        override_vim_notify = true,
-        window = {
-            winblend = 0,
-            border = "solid",
-        },
-    },
-    progress = {
-        display = {
-            done_icon = "✓", -- Icon shown when LSP task finishes
-        },
-    },
+	notification = {
+		override_vim_notify = true,
+		window = {
+			winblend = 0,
+			border = "solid",
+		},
+	},
+	progress = {
+		display = {
+			done_icon = "✓", -- Icon shown when LSP task finishes
+		},
+	},
 })
 
 -- require("mini.pick").setup()
@@ -144,55 +142,79 @@ require("mini.pairs").setup()
 require("mini.icons").setup()
 require("mini.bufremove").setup()
 
-local ts_parsers = { "lua", "html", "css", "javascript", "typescript", "python", "go", "rust", "dockerfile", "vim",
-    "vimdoc", "json", "bash" }
-local servers = { "lua_ls", "ts_ls", "html", "cssls", "rust_analyzer", "gopls", "emmet_language_server", "jsonls",
-    "dockerls", "tombi", "bashls" }
+local ts_parsers = {
+	"lua",
+	"html",
+	"css",
+	"javascript",
+	"typescript",
+	"python",
+	"go",
+	"rust",
+	"dockerfile",
+	"vim",
+	"vimdoc",
+	"json",
+	"bash",
+}
+local servers = {
+	"lua_ls",
+	"ts_ls",
+	"html",
+	"cssls",
+	"rust_analyzer",
+	"gopls",
+	"emmet_language_server",
+	"jsonls",
+	"dockerls",
+	"tombi",
+	"bashls",
+}
 
 require("nvim-treesitter").install(ts_parsers)
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = servers
+	ensure_installed = servers,
 })
 vim.lsp.enable(servers)
 
 require("blink-cmp").setup({
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-    completion = {
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 500, -- Delay before showing
-        },
-        ghost_text = {
-            enabled = true, -- Show completion as ghost text
-        },
-    },
-    sources = {
-        providers = {
-            snippets = {
-                opts = {
-                    friendly_snippets = true,
-                    extended_filetypes = {
-                        markdown = { 'jekyll' },
-                        sh = { 'shelldoc' },
-                        php = { 'phpdoc' },
-                        cpp = { 'unreal' }
-                    }
-                }
-            }
-        }
-    }
+	fuzzy = { implementation = "prefer_rust_with_warning" },
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 500, -- Delay before showing
+		},
+		ghost_text = {
+			enabled = true, -- Show completion as ghost text
+		},
+	},
+	sources = {
+		providers = {
+			snippets = {
+				opts = {
+					friendly_snippets = true,
+					extended_filetypes = {
+						markdown = { "jekyll" },
+						sh = { "shelldoc" },
+						php = { "phpdoc" },
+						cpp = { "unreal" },
+					},
+				},
+			},
+		},
+	},
 })
 require("lazydev").setup()
 
 require("oil").setup({
-    default_file_explorer = true,
-    view_options = {
-        show_hidden = true
-    },
-    win_options = {
-        winblend = 0
-    }
+	default_file_explorer = true,
+	view_options = {
+		show_hidden = true,
+	},
+	win_options = {
+		winblend = 0,
+	},
 })
 
 require("configs.autocmds")
